c6f1cca2ba88a01543706ed3b14db4ed4a2bb334
Frantisek Hrbata <qwertyn@redirfs.org>
2007-08-08 09:51:13 +0000

    + av_test uses pthreads

--- src/avflt/lib/test/av_test.c
+++ src/avflt/lib/test/av_test.c
@@ -2,7 +2,12 @@
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
+#include <unistd.h>
 
+#define THREADS_NUM 10
+
+struct av_con avc;
 int intr = 0;
 
 void sighandler(int sig)
@@ -10,53 +15,41 @@ void sighandler(int sig)
 	intr = 1;
 }
 
-int main(int argc, char *argv[])
+void *check(void *data)
 {
-	struct sigaction sa;
-	struct av_con avc;
 	struct av_req avr;
+	sigset_t sigmask;
 	const char *fn;
 	const char *en;
 	int event;
 	int rv;
 
-	memset(&sa, 0, sizeof(struct sigaction));
-	sa.sa_handler = sighandler;
-	sigemptyset(&sa.sa_mask);
-	sigaddset(&sa.sa_mask, SIGTERM);
-	sigaddset(&sa.sa_mask, SIGINT);
-	sigaction(SIGTERM, &sa, NULL);
-	sigaction(SIGINT, &sa, NULL);
+	sigemptyset(&sigmask);
+	sigaddset(&sigmask, SIGINT);
+	sigaddset(&sigmask, SIGTERM);
+	pthread_sigmask(SIG_BLOCK, &sigmask, NULL);
 
-	rv = av_register(&avc);
-	if (rv) {
-		fprintf(stderr, "av_register failed: %s(%d)\n", strerror(rv), rv);
-		return 1;
-	}
-
-	rv = av_include("/tmp");
-	if (rv) {
-		fprintf(stderr, "av_include failed: %s(%d)\n", strerror(rv), rv);
-		return 1;
-	}
+	sigemptyset(&sigmask);
+	sigaddset(&sigmask, SIGUSR1);
+	pthread_sigmask(SIG_UNBLOCK, &sigmask, NULL);
 
 	while (!intr) {
 		rv = av_request(&avc, &avr);
 		if (rv) {
 			fprintf(stderr, "av_request failed: %s(%d)\n", strerror(rv), rv);
-			return 1;
+			return NULL;
 		}
 
 		rv = av_get_filename(&avr, &fn);
 		if (rv) {
 			fprintf(stderr, "av_get_filename failed: %s(%d)\n", strerror(rv), rv);
-			return 1;
+			goto reply;
 		}
 
 		rv = av_get_event(&avr, &event);
 		if (rv) {
 			fprintf(stderr, "av_get_event failed: %s(%d)\n", strerror(rv), rv);
-			return 1;
+			goto reply;
 		}
 
 		switch (event) {
@@ -76,23 +69,83 @@ int main(int argc, char *argv[])
 				en = "UNKNOWN";
 		}
 
-		printf("access control: %s: %s\n", en, fn);
+		printf("%ld: access control: %s: %s\n", pthread_self(), en, fn);
 
 		rv = av_set_access(&avr, AV_ACCESS_ALLOW); 
 		if (rv) {
 			fprintf(stderr, "av_access failed: %s(%d)\n", strerror(rv), rv);
-			return 1;
+			goto reply;
 		}
 
 		rv = av_reply(&avc, &avr);
 		if (rv) {
 			fprintf(stderr, "av_reply failed: %s(%d)\n", strerror(rv), rv);
-			return 1;
+			goto reply;
 		}
+	}
+
+	return NULL;
+
+reply:
+	rv = av_reply(&avc, &avr);
+	if (rv)
+		fprintf(stderr, "av_reply in exit failed: %s(%d)\n", strerror(rv), rv);
+
+	return NULL;
+
+}
+
+int main(int argc, char *argv[])
+{
+	struct sigaction sa;
+	pthread_t threads[THREADS_NUM];
+	int rv;
+	int i;
+
+	memset(&sa, 0, sizeof(struct sigaction));
+	sa.sa_handler = sighandler;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGTERM);
+	sigaddset(&sa.sa_mask, SIGINT);
+	sigaction(SIGTERM, &sa, NULL);
+	sigaction(SIGINT, &sa, NULL);
+
+	rv = av_register(&avc);
+	if (rv) {
+		fprintf(stderr, "av_register failed: %s(%d)\n", strerror(rv), rv);
+		return 1;
+	}
+
+	rv = av_include("/tmp");
+	if (rv) {
+		av_unregister(&avc);
+		fprintf(stderr, "av_include failed: %s(%d)\n", strerror(rv), rv);
+		return 1;
+	}
+
+	for (i = 0; i < THREADS_NUM; i++) {
+		rv = pthread_create(&threads[i], NULL, check, NULL);
+		if (rv)
+			fprintf(stderr, "pthread_create failed: %s(%d)\n", strerror(rv), rv);
+	}
+
+	pause();
+
+	for (i = 0; i < THREADS_NUM; i++) {
+		rv = pthread_kill(threads[i], SIGUSR1);
+		if (rv)
+			fprintf(stderr, "pthread_kill failed: %s(%d)\n", strerror(rv), rv);
+	}
+
+	for (i = 0; i < THREADS_NUM; i++) {
+		rv = pthread_join(threads[i], NULL);
+		if (rv)
+			fprintf(stderr, "pthread_join failed: %s(%d)\n", strerror(rv), rv);
 	}
 
 	rv = av_exclude("/tmp");
 	if (rv) {
+		av_unregister(&avc);
 		fprintf(stderr, "av_exclude failed: %s(%d)\n", strerror(rv), rv);
 		return 1;
 	}
