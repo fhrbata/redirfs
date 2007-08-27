243a22993b88015d43f645943d147666c410b846
Frantisek Hrbata <qwertyn@redirfs.org>
2007-08-27 15:01:42 +0000

    + added cache of scanned files(uses rfs_*_data_inode functions) + always scan file opened/mapped for writing otherwise check cache

--- src/avflt/avflt_rfs.c
+++ src/avflt/avflt_rfs.c
@@ -4,11 +4,18 @@
 #define AVFLT_REG_IOP_PERMISSION	1
 #define AVFLT_REG_FOP_RELEASE		2
 
+#define AVFLT_CLEAN			1
+#define AVFLT_INFECTED			2
+
+struct avflt_data {
+	struct rfs_priv_data rfs_data;
+	spinlock_t lock;
+	int state;
+};
+
+#define rfs_to_avflt_data(ptr) container_of(ptr, struct avflt_data, rfs_data)
+
 static int avflt_ctl(struct rfs_ctl *ctl);
-static enum rfs_retv avflt_pre_open(rfs_context context,
-		struct rfs_args *args);
-static enum rfs_retv avflt_pre_permission(rfs_context context,
-		struct rfs_args *args);
 
 rfs_filter avflt;
 static struct rfs_filter_info avflt_info = {"avflt", 666, 0, avflt_ctl};
@@ -17,6 +24,7 @@ int avflt_open = 1;
 int avflt_close = 1;
 int avflt_exec = 1;
 spinlock_t avflt_rops_lock = SPIN_LOCK_UNLOCKED;
+static struct kmem_cache *avflt_data_cache = NULL;
 
 static struct rfs_op_info avflt_rops[] = {
 	{RFS_REG_FOP_OPEN, NULL, NULL},
@@ -25,14 +33,113 @@ static struct rfs_op_info avflt_rops[] = {
 	{RFS_OP_END, NULL, NULL}
 };
 
+static void avflt_data_free(struct rfs_priv_data *rfs_data)
+{
+	struct avflt_data *data = rfs_to_avflt_data(rfs_data);
+
+	kmem_cache_free(avflt_data_cache, data);
+}
+
+static struct avflt_data *avflt_data_alloc(void)
+{
+	struct avflt_data *data;
+	int err;
+
+	data = kmem_cache_alloc(avflt_data_cache, GFP_KERNEL);
+	if (!data)
+		return ERR_PTR(-ENOMEM);
+
+	err = rfs_init_data(&data->rfs_data, avflt, avflt_data_free);
+	if (err) {
+		 kmem_cache_free(avflt_data_cache, data);
+		 return ERR_PTR(err);
+	}
+
+	data->state = 0;
+	spin_lock_init(&data->lock);
+
+	return data;
+}
+
+static inline struct avflt_data *avflt_get_data(struct inode *inode)
+{
+	struct rfs_priv_data *rfs_data;
+
+	int err;
+
+	err = rfs_get_data_inode(avflt, inode, &rfs_data);
+	if (err)
+		return ERR_PTR(err);
+
+	return rfs_to_avflt_data(rfs_data);
+}
+
+static inline void avflt_put_data(struct avflt_data *data)
+{
+	rfs_put_data(&data->rfs_data);
+}
+
+static inline struct avflt_data *avflt_attach_data(struct inode *inode)
+{
+	struct avflt_data *data;
+	struct rfs_priv_data *data_exist;
+	int err;
+
+	data = avflt_get_data(inode);
+	if (!IS_ERR(data))
+		return data;
+
+	if (PTR_ERR(data) != -ENODATA)
+		return data;
+
+	data = avflt_data_alloc();
+	if (IS_ERR(data)) 
+		return data;
+
+	err = rfs_attach_data_inode(avflt, inode, &data->rfs_data, &data_exist);
+	if (err) {
+		if (err == -EEXIST) {
+			avflt_put_data(data);
+			data = rfs_to_avflt_data(data_exist);
+
+		} else {
+			avflt_put_data(data);
+			return ERR_PTR(err);
+		}
+	}
+
+	return data;
+}
+
 static enum rfs_retv avflt_event(struct dentry *dentry, int event,
 		struct rfs_args *args)
 {
 	struct avflt_check *check = NULL;
+	struct avflt_data *data = NULL;
 	int rv;
+	int state;
 
 	if (avflt_pid_find(current->pid))
 		return RFS_CONTINUE;
+
+	if (!atomic_read(&dentry->d_inode->i_writecount)) {
+		data = avflt_get_data(dentry->d_inode);
+		if (!IS_ERR(data)) {
+			spin_lock(&data->lock);
+			state = data->state;
+			spin_unlock(&data->lock);
+
+			if (state == AVFLT_CLEAN) {
+				avflt_put_data(data);
+				return RFS_CONTINUE;
+
+			} else {
+				avflt_put_data(data);
+				args->retv.rv_int = -EPERM;
+				return RFS_STOP;
+			}
+		}
+	}
 
 	rv = avflt_request_wait();
 
@@ -78,17 +185,35 @@ static enum rfs_retv avflt_event(struct dentry *dentry, int event,
 		avflt_check_put(check);
 		return RFS_STOP;
 	}
+	
+	data = avflt_attach_data(dentry->d_inode);
+	if (IS_ERR(data)) {
+		printk(KERN_WARNING "avflt_attach_data failed(%ld)\n",
+				PTR_ERR(data));
+		data = NULL;
+	}
 
 	if (atomic_read(&check->deny)) {
-		avflt_check_put(check);
 		args->retv.rv_int = -EPERM;
-		return RFS_STOP;
+		state = AVFLT_INFECTED;
+		rv = RFS_STOP;
+
+	} else {
+		args->retv.rv_int = 0;
+		state = AVFLT_CLEAN;
+		rv = RFS_CONTINUE;
+	}
+
+	if (data) {
+		spin_lock(&data->lock);
+		data->state = state;
+		spin_unlock(&data->lock);
+		avflt_put_data(data);
 	}
 
 	avflt_check_put(check);
-	args->retv.rv_int = 0;
 
-	return RFS_CONTINUE;
+	return rv;
 }
 
 static enum rfs_retv avflt_pre_open(rfs_context context, struct rfs_args *args)
@@ -163,13 +288,37 @@ int avflt_rfs_set_ops(void)
 	return rfs_set_operations(avflt, avflt_rops);
 }
 
+static int avflt_rfs_data_cache_init(void)
+{
+	avflt_data_cache = kmem_cache_create("avflt_data_cache",
+			sizeof(struct avflt_data),
+			0, SLAB_RECLAIM_ACCOUNT,
+			NULL, NULL);
+
+	if (!avflt_data_cache)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static void avflt_rfs_data_cache_exit(void)
+{
+	kmem_cache_destroy(avflt_data_cache);
+}
+
 int avflt_rfs_init(void)
 {
 	int err;
 
-	err = rfs_register_filter(&avflt, &avflt_info);
+	err = avflt_rfs_data_cache_init();
 	if (err)
 		return err;
+
+	err = rfs_register_filter(&avflt, &avflt_info);
+	if (err) {
+		avflt_rfs_data_cache_exit();
+		return err;
+	}
 
 	spin_lock(&avflt_rops_lock);
 	err = avflt_rfs_set_ops();
@@ -185,6 +334,8 @@ int avflt_rfs_init(void)
 
 error:
 	rfs_unregister_filter(avflt);
+	avflt_rfs_data_cache_exit();
+
 	return err;
 }
 
@@ -195,6 +346,8 @@ void avflt_rfs_exit(void)
 	err = rfs_unregister_filter(avflt);
 	if (err)
 		printk(KERN_ERR "avflt: rfs_unregister_filter failed(%d)\n", err);
+
+	avflt_rfs_data_cache_exit();
 }
 
 
