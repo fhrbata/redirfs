c42754437e8dc678e3f0c75d4f3e54ae4bd25630
Frantisek Hrbata <qwertyn@redirfs.org>
2008-11-18 17:42:28 +0000

    + use av_request with timeout, it allows user-space applications nicely exit(no need for sending signals to threads)

--- src/avtest/avtest.c
+++ src/avtest/avtest.c
@@ -13,36 +13,35 @@
 static struct av_connection av_conn;
 static int stop = 0;
 
-void sighandler(int sig)
+static void sighandler(int sig)
 {
 	stop = 1;
 }
 
-static void *check(void *data)
+static int check(void)
 {
-	sigset_t sigmask;
 	struct av_event av_event;
 	char fn[PATH_MAX];
 
-	sigemptyset(&sigmask);
-	sigaddset(&sigmask, SIGINT);
-	sigaddset(&sigmask, SIGTERM);
-	pthread_sigmask(SIG_BLOCK, &sigmask, NULL);
-
 	while (!stop) {
-		if (av_request(&av_conn, &av_event)) {
+		if (av_request(&av_conn, &av_event, 500)) {
+			if (errno == ETIMEDOUT) {
+				printf("timeout\n");
+				continue;
+			}
+
 			perror("av_request failed");
-			return NULL;
+			return -1;
 		}
 
 		if (av_get_filename(&av_event, fn, PATH_MAX)) {
-			perror("av_request failed");
-			return NULL;
+			perror("av_get_filename failed");
+			return -1;
 		}
 
 		if (av_set_result(&av_event, AV_ACCESS_ALLOW)) {
-			perror("av_request failed");
-			return NULL;
+			perror("av_set_result failed");
+			return -1;
 		}
 
 		printf("thread[%lu]: id: %d, type: %d, fd: %d, pid: %d, "
@@ -51,11 +50,27 @@ static void *check(void *data)
 				av_event.pid, av_event.tgid, av_event.res, fn);
 
 		if (av_reply(&av_conn, &av_event)) {
-			perror("av_request failed");
-			return NULL;
+			perror("av_reply failed");
+			return -1;
 		}
 
 	}
+
+	return 0;
+}
+
+static void *check_thread(void *data)
+{
+	sigset_t sigmask;
+
+	sigemptyset(&sigmask);
+	sigaddset(&sigmask, SIGINT);
+	sigaddset(&sigmask, SIGTERM);
+	pthread_sigmask(SIG_BLOCK, &sigmask, NULL);
+
+	if (check())
+		fprintf(stderr, "thread[%lu] unexpectedly stopped, %s\n",
+				pthread_self(), strerror(errno));
 
 	return NULL;
 }
@@ -82,7 +97,7 @@ int main(int argc, char *argv[])
 	}
 
 	for (i = 0; i < THREADS_COUNT; i++) {
-		rv = pthread_create(&threads[i], NULL, check, NULL);
+		rv = pthread_create(&threads[i], NULL, check_thread, NULL);
 		if (rv) {
 			fprintf(stderr, "pthread_create failed: %d\n", rv);
 			exit(EXIT_FAILURE);
